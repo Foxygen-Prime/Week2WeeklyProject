5d5e9fc9ac6557b2e85b73b9a203eae8435a69ce
Courtney <courtney.trochim@gmail.com>
2017-07-29 18:31:11 -0500

    Working through problems, first two should be good, up to 4 or 5

--- starter_files/main.js
+++ starter_files/main.js
@@ -1,4 +1,4 @@
-// Since the `data.js` file is loaded in your `index.html` before this one, 
+// Since the `data.js` file is loaded in your `index.html` before this one,
 // you have access to the `data` variable that contains the Etsy Store data.
 // Open your `index.html` page in your browser and you can verify that the following
 // is properly working. The `data` variable is an array with 25 items in it
@@ -7,40 +7,65 @@ console.log(data);
 
 
 // 1: Show me how to calculate the average price of all items.
-function question1 () {
+
+let avg = "";
+
+function question1() {
   // Answer:
+  for (let i = 0; i < data.length; i++) {
+    (avg += data[i])
+  }
+  avg = avg / data.length;
 }
+console.log(avg)
 
 
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
-  // Answer:
+let midPricing = [];
+
+function question2() {
+  for (let i = 0; i < data.length; i++) {
+    if (i >= 14 && i <= 18) {
+      midPricing.push(data[i]);
+    }
+  }
 }
+console.log(midPricing);
 
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
+function question3() {
   // Answer:
+  let answer3 = string.match(data["GBP"])
 }
-
+console.log(answer3);
+//come back to this one, not sure what to do
 
 // 4: Display a list of all items who are made of wood.
-function question4 () {
+
+let woodProducts = [];
+
+function question4() {
   // Answer:
+  for (if i = 0; i < data.length; i++) {
+    woodProducts.push(data.materials["wood"]);
+  }
+
 }
+console.log(woodProducts);
 
 
-// 5: Which items are made of eight or more materials? 
+// 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
-function question5 () {
+function question5() {
   // Answer:
 }
 
 
 // 6: How many items were made by their sellers?
 // Answer:
-function question6 () {
+function question6() {
   // Answer:
 }
