23129d718d09cce936dab3e449da5f4fcd03aeb9
Courtney <courtney.trochim@gmail.com>
2017-08-03 17:09:15 -0500

    Fixed logs

--- starter_files/main.js
+++ starter_files/main.js
@@ -15,7 +15,7 @@ let avg = 0;
     (avg += data[i].price);
   }
   avg = avg / data.length;
-  console.log(question1)
+  console.log(avg)
 }
 
 
@@ -30,7 +30,7 @@ function question2() {
       midPricing.push(data[i].title)
     }
   }
-  console.log(question2);
+  console.log(midPricing);
 }
 
 
@@ -40,10 +40,10 @@ function question3() {
   let answer3 = [];
   for (let i = 0; i < data.length; i++) {
     if (data[i].currency_code === "GBP") {
-      answer3.push(data.title + data.price)
+      answer3.push(data[i].title + data[i].price)
     }
   }
-  console.log(question3);
+  console.log(answer3);
 }
 
 
@@ -54,11 +54,15 @@ function question4() {
   // Answer:
   let woodProducts = [];
   for (let i = 0; i < data.length; i++) {
-    if (data[i].materials === "wood") {
-      woodProducts.push(data.title)
+    // console.log(data[i].materials);
+    // console.log(typeof data[i].materials);
+    for (let j = 0; j < data[i].materials.length; j++) {
+    if (data[i].materials[j] === "wood") {
+      woodProducts.push(data[i].title)
+        }
     }
   }
-  console.log(question4);
+  console.log(woodProducts);
 }
 
 
@@ -69,11 +73,12 @@ function question5() {
   // Answer:
   let itemComponents = [];
   for (i = 0; i < data.length; i++) {
-    if (data[i].length >= 8) {
-      itemComponents.push(data[i].title, data[i].materials.length, data[i].items);
-    }
+    if (data[i].materials.length >= 8) {
+      itemComponents.push(data[i].title, data[i].materials.length, data[i].materials);
+      }
+
   }
-console.log(question5);
+console.log(itemComponents);
 }
 
 // 6: How many items were made by their sellers?
@@ -83,10 +88,10 @@ function question6() {
   let homeMade = [];
   for (let i = 0; i < data.length; i++) {
     if (data[i].who_made === "i_did") {
-      homeMade.push(data[i].item)
+      homeMade.push(data[i].title)
     }
   }
+  console.log(homeMade.length);
   return homeMade.length;
 
 }
-console.log(question6);
